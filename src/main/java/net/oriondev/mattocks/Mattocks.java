14b6f6abdb1352b9ec78669e072b4f778ae4b979
HexBugOrion <58091259+HexBugOrion@users.noreply.github.com>
2020-10-26 16:06:24 -0500

    Revert "Merge pull request #1 from iDoomful/master"
    This reverts commit ea4091833d60856af3f9cca6248240af1df80686, reversing
    changes made to 59759cc42e7621ac612b43cb57f20c040146b477.
    
    # Conflicts:
    #	build.gradle
    #	gradle.properties
    #	src/main/java/net/oriondev/mattocks/Mattocks.java
    #	src/main/resources/fabric.mod.json

--- src/main/java/net/oriondev/mattocks/Mattocks.java
+++ src/main/java/net/oriondev/mattocks/Mattocks.java
@@ -1,23 +1,31 @@
 package net.oriondev.mattocks;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
+import net.fabricmc.loader.FabricLoader;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ToolMaterials;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.oriondev.mattocks.mattock.EnderiteMaterial;
 import net.oriondev.mattocks.mattock.Mattock;
 
 public class Mattocks implements ModInitializer {
-	public static final Mattock NETHERITE_MATTOCK = new Mattock(5, -3, ToolMaterials.NETHERITE, new Item.Settings().group(ItemGroup.TOOLS));
-	public static final Mattock DIAMOND_MATTOCK = new Mattock(3, -3, ToolMaterials.DIAMOND, new Item.Settings().group(ItemGroup.TOOLS));
-	public static final Mattock IRON_MATTOCK = new Mattock(3, -3, ToolMaterials.IRON, new Item.Settings().group(ItemGroup.TOOLS));
+
+	public static final Mattock ENDERITE_MATTOCK = new Mattock(7, -3, new EnderiteMaterial(), new Item.Settings().group(ItemGroup.TOOLS).fireproof());
+	public static final Mattock NETHERITE_MATTOCK = new Mattock(6, -3, ToolMaterials.NETHERITE, new Item.Settings().group(ItemGroup.TOOLS).fireproof());
+	public static final Mattock DIAMOND_MATTOCK = new Mattock(5, -3, ToolMaterials.DIAMOND, new Item.Settings().group(ItemGroup.TOOLS));
+	public static final Mattock IRON_MATTOCK = new Mattock(4, -3, ToolMaterials.IRON, new Item.Settings().group(ItemGroup.TOOLS));
 	public static final Mattock STONE_MATTOCK = new Mattock(3, -3, ToolMaterials.STONE, new Item.Settings().group(ItemGroup.TOOLS));
-	public static final Mattock WOODEN_MATTOCK = new Mattock(2, -3, ToolMaterials.WOOD, new Item.Settings().group(ItemGroup.TOOLS));
 	public static final Mattock GOLDEN_MATTOCK = new Mattock(3, -3, ToolMaterials.GOLD, new Item.Settings().group(ItemGroup.TOOLS));
+	public static final Mattock WOODEN_MATTOCK = new Mattock(2, -3, ToolMaterials.WOOD, new Item.Settings().group(ItemGroup.TOOLS));
+
 	@Override
 	public void onInitialize() {
+		if(FabricLoader.INSTANCE.isModLoaded("enderitemod")) {
+			Registry.register(Registry.ITEM, new Identifier("mattocks", "enderite_mattock"), ENDERITE_MATTOCK);
+		}
+
 		Registry.register(Registry.ITEM, new Identifier("mattocks", "netherite_mattock"), NETHERITE_MATTOCK);
 		Registry.register(Registry.ITEM, new Identifier("mattocks", "diamond_mattock"), DIAMOND_MATTOCK);
 		Registry.register(Registry.ITEM, new Identifier("mattocks", "iron_mattock"), IRON_MATTOCK);
--- src/main/java/net/oriondev/mattocks/mattock/Mattock.java
+++ src/main/java/net/oriondev/mattocks/mattock/Mattock.java
@@ -1,5 +1,6 @@
 package net.oriondev.mattocks.mattock;
 
+
 import net.minecraft.block.BlockState;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.MiningToolItem;
